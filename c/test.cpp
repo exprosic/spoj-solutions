aa5cfd20c46000a2ccca47c96c1378d61d80d062
exprosic <exprosic@hotmail.com>
2016-04-02 12:08:50 +0800

    +test

---
+++ c/test.cpp
@@ -0,0 +1,12 @@
+// vim: set noet ts=4 sw=4 fileencoding=utf-8:
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k;
+	while (cin >>k)
+		if (k==42)
+			return 0;
+		else
+			cout <<k <<endl;
+}
